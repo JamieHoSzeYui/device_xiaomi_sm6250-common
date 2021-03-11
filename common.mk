fab10d9b9ab6424ae873408b678a3e622938301d
Sonal Singh <sonal.singh.19993@gmail.com>
2021-03-11 11:42:17 +0000

    sm6250-common: Guard tree with soong namespace  * prevent module conflicts with other device trees
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -358,9 +358,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/xiaomi
+    kernel/xiaomi/sm6250
 
 # Telephony
 PRODUCT_PACKAGES += \

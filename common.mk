524d44d04a3db96f449ac18b09ae05fe750f098b
JamieHoSzeYui <henloboii@protonmail.ch>
2021-04-02 12:08:50 +0800

    Revert "sm6250-common: Add Pixel power stats HAL from sunfish"
    This reverts commit bffe44f4129e7484fff126645f7fc8e73f918ab0.

--- common.mk
+++ common.mk
@@ -302,8 +302,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sm6250-libperfmgr \
-    android.hardware.power.stats@1.0-service.xiaomi_sm6250
+    android.hardware.power@1.3-service.xiaomi_sm6250-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
